5ff7f99a219fdf93835c8d33aa2bc138b7a1f853
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-08 10:31:21 +0430

    controllers and routes created but api cannot GET

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-
+const tasks = require('./routes/tasks')
 const port = 8000
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('setup completed')
 })
+
+app.use('api/tasks', tasks)
 
 app.listen(port, () => {
     console.log(`server listening on port ${port}`)
---
+++ task-manager/controllers/tasks.js
@@ -0,0 +1,9 @@
+const getAllTasks = (req, res) => {
+    res.send('Task')
+}
+
+const createTask = (req, res) => {
+    res.send('create Task')
+}
+
+module.exports = { getAllTasks }
