69cf14c68779952d0528a22c5e2a0f5866300aee
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-14 21:53:41 +0430

    jobs api project completed

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -3,6 +3,8 @@ require('express-async-errors');
 
 const helmet = require("helmet")
 const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 
 const express = require('express');
 const app = express();
@@ -17,9 +19,18 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+  })
+)
+
 app.use(express.json());
 app.use(helmet())
 app.use(cors())
+app.use(xss())
 // extra packages
 
 // routes
