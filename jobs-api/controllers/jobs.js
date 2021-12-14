512e0e47f3bbb6916c1707089672cb8c37520c80
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-14 21:41:05 +0430

    jobs api created and errors for better user experience

--- jobs-api/controllers/jobs.js
+++ jobs-api/controllers/jobs.js
@@ -1,12 +1,22 @@
 const Job = require('../models/Job')
 const { StatusCodes } = require('http-status-codes')
+const { NotFoundError, BadRequestError } = require('../errors')
 
 const getAllJobs = async (req, res) => {
-
-
+    const job = await Job.find({createdBy: req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({job, count: jobs.length})
 }
 const getJob = async (req, res) => {
+    const {user:{userId}, params: {id: jobId}} = req
+    const job = await Job.findOne({
+        _id: jobId,
+        createdBy: userId
+    })
 
+    if(!jub){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({job})
 }
 const createJob = async (req, res) => {
     req.body.createdBy = req.user.userId
@@ -14,10 +24,31 @@ const createJob = async (req, res) => {
     res.status(StatusCodes.CREATED).json({job})
 }
 const updateJob = async (req, res) => {
+    const {body:{company, position} ,user:{userId}, params: {id: jobId}} = req
+    if(company === '' || position === ''){
+        throw new BadRequestError('Company or Position fields cannot be empty')
+    }
+    const job = await Job.findOneAndUpdate({
+        _id: jobId,
+        createdBy: userId
+    }, req.body, {new:true, runValidators: true})
 
+    if(!jub){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({job})
 }
 const deleteJob = async (req, res) => {
+    const {user:{userId}, params: {id: jobId}} = req
+    const job = await Job.findOneAndRemove({
+        _id: jobId,
+        createdBy: userId
+    })
 
+    if(!jub){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json()
 }
 
 module.exports = {getAllJobs, getJob, createJob, updateJob, deleteJob}
--- jobs-api/middleware/error-handler.js
+++ jobs-api/middleware/error-handler.js
@@ -1,10 +1,24 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong try again later'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+  if(err.name === 'ValidationError'){
+    customError.msg = Object.values(err.errors).map((item) => item.message).join(',')
+    customError.statusCode = 400
+  }
+  if (err.code && err.code == 11000) {
+    customError.msg = `Duplicate value enter for ${Object.keys(err.KeyValue)} field, please enter another value`
+    customError.statusCode = 400
+  }
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
